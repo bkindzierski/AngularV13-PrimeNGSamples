3f49d3c613ede2a1bca7c47f9b3f838a5b908f2b
bkindzierski@merchantsgroup.com <bkindzierski@merchantsgroup.com>
2022-03-01 10:35:55 -0500

    remove test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-//primeng test
+//primeng
 import {ButtonModule} from 'primeng/button';
 import {PanelModule} from 'primeng/panel';
 import {ToastModule} from 'primeng/toast';
